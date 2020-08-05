8852373908b43fccca4b95f5fe679cf69a06cb2c
Dmytro Safonov <dmytro.safonov@seznam.cz>
2020-08-05 20:34:41 +0200

    #1 Implemnt login routine. First look on "get balance" method.

--- opium_api/connector.py
+++ opium_api/connector.py
@@ -1,4 +1,16 @@
+from http import HTTPStatus
+from typing import Optional
+
+import requests
+
+from requests import Response
+
+from libs.py_eth_sig_utils.signing import v_r_s_to_signature, sign_typed_data
+
+
+from opium_api.enums import HttpMethod
 from opium_api.constants import API_VERSION, API_HOST
+from opium_api.exceptions import APIException, UnknownHttpMethod
 
 
 class Connector:
@@ -8,6 +20,7 @@ class Connector:
         if not public_key:
             raise ValueError('Empty "public_key"')
 
+        self.__api_url: str = f'https://{API_HOST}/{API_VERSION}'
         self.__access_token: str = ''
         self.__private_key: bytes = bytes.fromhex(private_key)
         self.__public_key: str = public_key
@@ -15,23 +28,83 @@ class Connector:
     def __signe_message(self):
         raise NotImplemented
 
-    def __make_secure_call(self):
-        raise NotImplemented
+    def __generate_access_token(self):
+        message_for_signing = self.__api_auth_logindata()
+        self.__access_token = v_r_s_to_signature(*sign_typed_data(message_for_signing, self.__private_key)).hex()
 
-    def __make_public_call(self):
-        raise NotImplemented
+    def __make_public_call(self,
+                           endpoint: str,
+                           method: HttpMethod,
+                           headers: Optional[dict] = None,
+                           arguments: Optional[dict] = None,
+                           data: Optional[dict] = None) -> Response:
+        headers = headers or dict()
+        arguments = arguments or dict()
+        data = data or dict()
 
-    def __api_auth_logindata(self):
+        api_url = f'{self.__api_url}{endpoint}'
+
+        if method == HttpMethod.get:
+            ret = requests.get(url=api_url, headers=headers, params=arguments)
+
+        elif method == HttpMethod.post:
+            ret = requests.post(url=api_url, headers=headers, params=arguments, json=data)
+
+        else:
+            raise UnknownHttpMethod
+
+        return ret
+
+    def __make_secure_call(self,
+                           endpoint: str,
+                           method: HttpMethod,
+                           arguments: Optional[dict] = None,
+                           data: Optional[dict] = None) -> Response:
+        if not self.__access_token:
+            self.__generate_access_token()
+
+        headers = {
+            'Authorization': f'Bearer 0x{self.__access_token}'
+        }
+
+        return self.__make_public_call(endpoint=endpoint,
+                                       method=method,
+                                       headers=headers,
+                                       arguments=arguments,
+                                       data=data)
+
+    def __api_auth_logindata(self) -> dict:
         """
         GET /auth/loginData
         """
-        raise NotImplemented
+        ret = self.__make_secure_call(endpoint='/auth/loginData', method=HttpMethod.get)
+
+        if ret.status_code != HTTPStatus.OK:
+            raise APIException('Unable to get login data')
+
+        return ret.json()
 
     def __api_wallet_balance_tokens(self):
         """
         GET /wallet/balance/tokens
         """
-        raise NotImplemented
+        arguments = {
+            'authAddress': self.__public_key
+
+        }
+
+        ret = self.__make_secure_call(endpoint='/wallet/balance/tokens',
+                                      method=HttpMethod.get,
+                                      arguments=arguments)
+
+        # TODO: Ask @Alirun
+        if ret.status_code == HTTPStatus.NO_CONTENT:
+            raise APIException('Wallet NO CONTENT -- Not implemented')
+
+        elif ret.status_code != HTTPStatus.OK:
+            raise APIException('Unknown API error')
+
+        return ret.json()
 
     def __api_orderbook_formorder(self):
         """
@@ -61,7 +134,8 @@ class Connector:
         raise NotImplemented
 
     def get_balance(self):
-        raise NotImplemented
+        # TODO: Think about return
+        return self.__api_wallet_balance_tokens()
 
     def send_order(self):
         raise NotImplemented
