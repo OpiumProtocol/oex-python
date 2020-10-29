e001cac9a357ef129c610d480c178740120e696c
KonstantinLitvin <konstantinlitvin89@gmail.com>
2020-10-29 11:42:29 +0300

    added cancel_order_test(...)

--- tests/client_test.py
+++ tests/client_test.py
@@ -7,41 +7,32 @@ from opium_api.enums import OrderBookAction
 from opium_sockets import OpiumApi
 
 
-# api = OpiumApi(test_api=True)
+client = OpiumClient(read_config('public_key'), read_config('private_key'))
 
 
 def get_balance_test():
-    client = OpiumClient(read_config('public_key'), read_config('private_key'))
 
     print(f"balance: {client.get_balance()}")
 
 
-def limit_buy_sell_test():
-
-    client = OpiumClient(read_config('public_key'), read_config('private_key'))
+def test_limit_buy_sell():
 
     # order: [{'id': '5f92b180603bd50028f46a82'}]
     order = client.create_order('OEX-FUT-1DEC-135.00',
                                 side='BUY',
-                                price=Decimal('20.43'),
-                                quantity=10)
+                                price=Decimal('19.43'),
+                                quantity='10')
     print(f"order: {order}")
     #
     # print(f"balance: {client.get_balance()}")
 
 
-# def t():
-#
-#     arguments = {
-#         'authAddress': public_key,
-#     }
-#
-#     r = client.make_secure_call('/trades/all/address', arguments=arguments)
-#     pprint(r.json())
-#
-#     r = client.make_secure_call('/tickers', arguments=arguments)
-#     pprint(r.json())
+def test_cancel_order():
+    id_: str = '5f9a8043468dd6003aaba960'
+    r = client.cancel_order([id_])
+
+    print(f"r: {r}")
 
 
 if __name__ == '__main__':
-    limit_buy_sell_test()
+    test_cancel_order()
